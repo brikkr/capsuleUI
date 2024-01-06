570b4d684964b8ec40cd748ad4846cecf4cebaa5
CerebrumJS <36863195+brikkr@users.noreply.github.com>
2024-01-06 18:05:04 +0100

    Add modal component

--- js/capsule-ui.js
+++ js/capsule-ui.js
@@ -1,9 +1,21 @@
-function toggleSidebar() {
-    const sidebar = document.querySelector('.sidebar');
-    const arrow = document.querySelector('.arrow');
+function toggleSidebar(select) {
+    let sidebar 
+    if(select == 'primary'){
+         sidebar = document.querySelector('.sidebar.primary');
+    }else{
+         sidebar = document.querySelector('.sidebar.secondary');
+    }
     if(sidebar.classList.contains('visible')){
         sidebar.classList.remove('visible');
     }else{
         sidebar.classList.add('visible');
+    }
+}
+
+function startLoading(btn) {
+    if(btn.classList.contains('loading')){
+        btn.classList.remove('loading');
+    }else{
+        btn.classList.add('loading');
     }
 }
